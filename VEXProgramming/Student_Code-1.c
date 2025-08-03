2797859dce08df5311e45841c961642eb3834997
Dylan Parkin <151808064+DylanParkin@users.noreply.github.com>
2025-08-03 16:39:06 +1200

    Update Student_Code-1.c

--- VEXProgramming/Student_Code-1.c
+++ VEXProgramming/Student_Code-1.c
@@ -1,14 +1,3 @@
-/**
- * @file Student_Code.c
- * @author your name (you@domain.com)
- * @brief description of this file
- * @version 0.1
- * @date yyyy-mm-dd
- *
- * @copyright Copyright (c) 2023
- *
- */
-
 /* Libraries. DO NOT REMOVE */
 #include "main.h"
 #include <stdint.h>
@@ -16,18 +5,6 @@
 #include <string.h>
 #include <math.h>
 #include "Student_Code.h"
-
-// ---------------------- Defining physical robot parameters --------------------------
-// Update these numbers to match the physical robot (information found in the lab manual)
-int drivingWheelDiameter = 0;	    // diameter of the driving wheels [mm]
-int robotWidth = 0;					// width of the robot including the wheel thickness [mm]
-int wheelWidth = 0;					// width of the driving wheel [mm]
-double drivingWheelRatio = 0.0;	    // ratio of wheel shaft rotations to wheel motor shaft rotations
-double armRatio = 0.0;				// ratio of arm shaft rotations to arm motor shaft rotations
-double encCountPerRev = 0.0;	    // number of encoder ticks per 1 revolution of the motor shaft
-// ------------------------------------------------------------------------------------
-
-/* Write your code in the function below. You may add helper functions below the studentCode function. */
 
 //The main function for the robot to complete the assigned task; picking up the payload, dropping it on the target, and navigating back to the start zone
 void student_main()
@@ -91,8 +68,6 @@ void student_main()
 
 
 // ----------------------------------------------- Function defintions go here  -----------------------------------------------//
-// Don't forget to add your function prototypes to Student_Code.h
-
 
 //Drives a specified distance in mm (forward or reverse). Drives in a straight line with PI controller accuracy
 void drive_straight_distance(double distance, double Kp, double Ki)
@@ -442,6 +417,7 @@ void line_following()
     motorPower(LeftMotor, 0);
     motorPower(RightMotor, 0);
 }
+
 
 
 
